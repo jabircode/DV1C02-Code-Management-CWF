198bd94881f0605449a1f98c7675028f3c7f7216
jabircode <jabirjamil@yahoo.com.sg>
2023-06-05 09:22:01 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent form submission
+  
+    // Handle form submission logic here
+    // You can use AJAX or any other technique to handle the form data
+  });
+  
